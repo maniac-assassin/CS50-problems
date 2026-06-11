6b2ac674946130d55b30f5ab6531e200e98431b9
maniac-assassin <maniacassassin1711@gmail.com>
2026-06-12 00:18:23 +0530

    Delete faces.py

--- faces.py
+++
@@ -1,8 +0,0 @@
-def main():
-    text = input("Enter text: ").split()
-    emojis = {":)": "🙂", ":(": "🙁"}
-    result = " ".join(emojis.get(word, word) for word in text)
-    print("Converted:", result)
-
-if __name__ == "__main__":
-    main()
